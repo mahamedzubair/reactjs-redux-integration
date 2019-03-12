51f1effe0e556b6646dd12daf7e7309397f777a7
Mahamed Zubair <mahamed@nestlabs.com>
2019-03-12 18:48:27 +0530

    Sorting table fixes

--- src/app/components/UITable.js
+++ src/app/components/UITable.js
@@ -1,205 +1,275 @@
-import React, { Component, Fragment } from "react";
-import { translate } from "react-i18next";
-import { Link } from "react-router-dom";
-import MediaQuery from "react-responsive";
+import React, {Component, Fragment} from 'react';
+import PropTypes from 'prop-types';
 
-//@translate(['common', 'table'])
 class UITable extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      headers: this.props.headers,
-      data: this.props.data,
-      sort: {
-        column: "",
-        direction: "icon icon-arrow-up",
-        sortname: ""
-      },
-      isFilterData: [...this.props.data],
-      isFilters: false,
-      isLoaded: false,
+    this.state = this.ingressDataTransform(props);
+  }
+
+  ingressDataTransform = (props) => {
+    const state = {};
+
+    state['data'] = props.data;
+    state['sort'] = {
+      column: 'none',
+      direction: 'sorting_asc',
+      sortname: '',
     };
-  }
+    state['displayRows'] = props.displayRows > props.data.length ? props.data.length : props.displayRows;
 
-  static getDerivedStateFromProps(newProps, currentState) {
-    if (newProps.data !== currentState.data) {
-      return {
-        data: newProps.data,
-        isFilterData: [...newProps.data],
-        isLoaded: newProps.isLoaded
-      };
-    }
-    return null;
-  }
-
-  //SORTING FUNCTIONALITY
-  onSort = (event, sortKey, isSort, itemType) => {
-    const sortData = this.state.isFilterData;
-    let tableSort = this.state.sort;
-    
-    if (isSort) {
-      if (
-        tableSort.column &&
-        tableSort.column === sortKey &&
-        tableSort.direction === "icon icon-arrow-up"
-      ) {
-        tableSort = {
-          column: sortKey,
-          direction: "icon icon-arrow-down",
-          sortname: "descending"
-        };
-        
-        this.props.sortList(sortKey, 'desc');
-      } else {
-        tableSort = {
-          column: sortKey,
-          direction: "icon icon-arrow-up",
-          sortname: "ascending"
-        };
-        this.props.sortList(sortKey, 'asc');
-      }
-    }
-    this.setState({ isFilterData: sortData, sort: tableSort });
+    return state;
   };
 
-  render () {
-    const { t } = this.props;
-    const loadSize = this.props.isLoaded
-      ? this.state.isFilterData.length
-      : this.props.defaultRowDisplay;
+  componentDidUpdate = (prevProps) => {
+    if(prevProps !== this.props) {
+      this.setState(...this.state, {
+        data: this.props.data,
+        displayRows: this.props.displayRows
+      });
+    }
+  }
+
+  handleClick = (sortColumn) => {
+    console.log('sortColumn', this.state.sort)
+    const sortData = this.state.data;
+    let tableSort = this.state.sort;
+    
+
+    // The Intl.Collator object is a constructor for collators, objects that enable language sensitive string comparison.
+    const collator = new Intl.Collator(undefined, {numeric: true, sensitivity: 'base'});
+
+    if (tableSort.column && tableSort.column === sortColumn && tableSort.direction === 'sorting_asc') {
+      tableSort = {
+        column: sortColumn,
+        direction: 'sorting_desc',
+        sortname: 'descending',
+      };
+      sortData.sort((b, a) => {
+        return collator.compare(a[sortColumn], b[sortColumn]);
+      });
+      this.props.sortList(sortColumn, 'desc');
+    } else {
+      tableSort = {
+        column: sortColumn,
+        direction: 'sorting_asc',
+        sortname: 'ascending',
+      };
+      this.props.sortList(sortColumn, 'asc');
+      sortData.sort((a, b) => {
+        return collator.compare(a[sortColumn], b[sortColumn]);
+      });
+    }
+
+    this.setState(...this.state, {
+      data: sortData,
+      sort: tableSort
+    }, () => {
+      this.dispatchChoiceEvent();
+    });
+  };
+
+  dispatchChoiceEvent = () => {
+    if(this.props.onClick) {
+      this.props.onClick(this.state);
+    }
+  }
+
+  renderHeaderRow = (row) => {
+    const sort = this.state.sort;
+    return (
+      <thead>
+        <tr>
+          {
+            row.map((column) => {
+              let className = null;
+              let sortName = null;
+              let label = column.label;
+
+              // For multi-line label support with PropType Validation
+              if(typeof label === 'object' && !React.isValidElement(label)) {
+                label = '';
+              }
+
+              if(column.sort && sort.column) {
+                className =  sort.column === column.name ? sort.direction : 'sorting';
+                sortName = sort.column === column.name ? sort.sortname : 'none';
+                label = <button className="button naked">{label}</button>
+              }
+
+              return (
+                <th
+                  className={className}
+                  key={`${this.props.name}-${column.name}`}
+                  onClick={column.sort && this.handleClick.bind(this, column.name)}
+                  aria-sort={sortName}
+                  style={{width: column.width}}>
+                  {label}
+                </th>
+              );
+            })
+          }
+        </tr>
+      </thead>
+    );
+  };
+
+  rowRender = (row, headers, rowIndex) => {
+    if(this.props.rowRender) {
+      return this.props.rowRender(row, headers, rowIndex);
+    }
+
+    return(
+      <tr key={`${this.props.name}-${rowIndex}`}>
+        {
+          headers.map((header, index) => {
+            return this.cellRender(row, header, index);
+          })
+        }
+      </tr>
+    );
+  }
+
+  cellRender = (row, header, columnIndex) => {
+    const value = header.cellRender || header.key;
+
+    if(typeof value === 'function') {
+      return <td key={columnIndex} className={header.columnClass}>{value(row, header)}</td>;
+    }
+
+    return(
+      <td key={columnIndex} className={header.columnClass}>
+        {row[value]}
+      </td>
+    );
+  }
+
+  renderBodyRows = (rows) => {
+    const headers = this.props.headers;
+    const displayRows = this.state.displayRows;
+    const rowsLength = rows.length;
+    let bodyRows = [];
+
+    if(rowsLength) {
+      for(let i=0; i<rowsLength; i++) {
+        if(i >= displayRows) {
+          break;
+        }
+        bodyRows[i] = this.rowRender(rows[i], headers, i);
+      }
+    } else {
+      bodyRows = (
+        <tr>
+          <td colspan={headers.length} className="text-center">{this.props.noDataText}</td>
+        </tr>
+      );
+    }
+
+    return(
+      <tbody>
+        {bodyRows}
+      </tbody>
+    );
+  }
+
+  render() {
+    const props = this.props;
 
     return (
-              <Fragment>               
-                <table id={this.props.name} className="dataTable responsiveTable bottom-1x">
-                  <thead>
-                    <tr>
-                      {this.state.headers.map((item, index) => {
-                        let classname;
-                        let sortname;
-                        let label = item.label;
-                        if (item.sort) {
-                          classname = "icon icon-sort-ascend-to-descend";
-                          if (
-                            this.state.sort.column &&
-                            this.state.sort.column === item.key
-                          ) {
-                            classname = this.state.sort.direction;
-                          }
-                          sortname = "none";
-                          if (
-                            this.state.sort.column &&
-                            this.state.sort.column === item.key
-                          ) {
-                            sortname = this.state.sort.sortname;
-                          }
-                          label = <button className="button naked">{label}</button>
-                        }
-                        return (
-                          <th
-                            className={classname}
-                            rowSpan="1"
-                            colSpan="1"
-                            key={index}
-                            onClick={e => this.onSort(e, item.key, item.sort, item.type)}
-                            aria-sort={sortname}
-                          >
-                            {label}
-                          </th>
-                        );
-                      })}
-                      {this.props.showDetails &&
-                        <th>
-                          Show Details
-                        </th>
-                      }
-                    </tr>
-                  </thead>
-          <MediaQuery minDeviceWidth={640}>
-            {matches => {
-              if (matches) {
-                return (
-                  <tbody>
-                    {this.state.isFilterData &&
-                      this.state.isFilterData
-                        .slice(0, loadSize)
-                        .map((rowData, index) => (
-                          <tr key={index}
-                          className={
-                            rowData.claimAppealed ? "claim-active" : ""
-                          }>
-                            {this.state.headers.map((obj, dataIndex, arr) => (
-                              <td
-                                key={dataIndex}
-                                data-title={rowData[obj.key]}
-                                className={obj.columnClass}
-                              >
-                              {obj.key === "yourCost" && rowData.savedCost
-                                  ? <span className="desktop-view icon circledIconColoredSmall icon-money-fill-circle"></span>
-                                  : ""}
-                                {rowData[obj.key]}
-                                <br/>
-                                {obj.key === "status" && rowData.claimAppealed === "Y"
-                                  ? 
-                                  <a href="#" className="desktop-view icon">
-                                    <span className="icon icon-1x icon-exclamation-circle-filled right-1x" aria-hidden="true" aria-label="warning" tabIndex="0"></span>
-                                    <span>Claimed</span>
-                                  </a>
-                                  : ""}
-                              </td>
-                            ))}
-                            {this.props.showDetails &&
-                              <td className="text-center">
-                                <a className="cursor-pointer" onClick={() => this.props.navigateToDetails(rowData)}>
-                                Show Details
-                                </a>
-                              </td>
-                            }
-                          </tr>
-                        ))}
-                  </tbody>
-                );
-              } else {
-                return ( 
-                <tbody>
-                  {this.state.isFilterData &&
-                      this.state.isFilterData
-                        .slice(0, loadSize)
-                        .map((rowData, index) => (
-                      <tr
-                        key={index}
-                        className={
-                          rowData.claimAppealed ? "claim-active" : ""
-                        }
-                      >                          
-                        {this.state.headers
-                        .slice(0, this.state.headers.length-1)
-                        .map((obj, dataIndex) => (
-                          <td
-                            key={dataIndex}
-                            data-title={rowData[obj.key]}
-                            className="pivoted"
-                          >
-                          <Link to={this.props.pageLink} className="details-view">  
-                            <div className="tdBefore">
-                              {obj.key === this.props.providerRowDisplay ? rowData[obj.key] : obj.name}
-                            </div>
-                            <span className={obj.key === this.props.providerRowDisplay ? 'provider-header' : ''}>
-                              {rowData[obj.key]}
-                            </span>
-                            </Link>
-                          </td>
-                        ))} 
-                      </tr>
-                    ))}
-                  </tbody>
-                  );
-                }
-              }}
-              </MediaQuery>
-              </table>
-              </Fragment>
-            );
-          }; 
+      <table id={props.name} aria-label={props.ariaLabel} className={props.className}>
+        {this.renderHeaderRow(props.headers)}
+        {this.renderBodyRows(this.state.data)}
+      </table>
+    );
+  }
+
+  /**
+   * Default Props for this component
+   */
+  static defaultProps = {
+    data: [],
+    displayRows: 10
+  }
+
+  static propTypes = {
+    /**
+     * Base for id calcuation should be unique on a page
+     * @type {[type]}
+     */
+    name: PropTypes.string.isRequired,
+    /**
+     * Class(es) for the Table
+     * @type {[type]}
+     */
+    className: PropTypes.string,
+    /**
+     * Table Headers
+     * @type {[type]}
+     */
+    headers: PropTypes.arrayOf(
+      PropTypes.shape({
+        /**
+         * Label name for header column
+         */
+        label: PropTypes.oneOfType([
+          PropTypes.string,
+          PropTypes.object // Not exactly object, React converts HTML to React Objects
+        ]).isRequired,
+        /**
+         * Base for header column id calcuation should be unique for table and used for sorting
+         */
+        name: PropTypes.string.isRequired,
+        /**
+         * This value should be grabbed from the data object to render in the table
+         */
+        key: PropTypes.string,
+        /**
+         * Enabling sorting feature
+         */
+        sort: PropTypes.bool,
+        /**
+         * Render custom cell
+         */
+        cellRender: PropTypes.func,
+        /**
+         * Class(es) for columns
+         */
+        columnClass: PropTypes.string,
+        /**
+         * What to show when the table has no data.
+         * @type {[type]}
+         */
+        noDataText: PropTypes.any
+      })
+    ).isRequired,
+    /**
+     * Table Data (Object key value pairs in a array)
+     * @type {[type]}
+     */
+    data: PropTypes.arrayOf(
+      PropTypes.shape({
+
+      })
+    ).isRequired,
+    /**
+     * For screen reader users.
+     * @type {[type]}
+     */
+    ariaLabel: PropTypes.string.isRequired,
+    /**
+     * Display no. of rows in table
+     * @type {[type]}
+     */
+    displayRows: PropTypes.number,
+    /**
+     * Render Custom rows
+     * @type {[type]}
+     * @param {object} row
+     * @param {array} headers
+     * @param {number} rowIndex
+     */
+    rowRender: PropTypes.func
+  };
 }
 
- export default UITable;
+export default UITable;
